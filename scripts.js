26cd93135d9330eafe1e797892e2d9d90342579d
RH90 <hasanaj11@hotmail.com>
2021-03-18 15:31:43 +0100

    2021-03-18_1531

--- scripts.js
+++ scripts.js
@@ -51,77 +51,78 @@ async function changePassword() {
 
 function createDatabase() {
 	console.log("Create Database");
-	var query =
-		'CREATE TABLE IF NOT EXISTS "history" (\n' +
-		'\t"id"\tINTEGER,\n' +
-		'\t"owner_table"\tINTEGER DEFAULT -1,\n' +
-		'\t"owner_id"\tINTEGER DEFAULT -1,\n' +
-		'\t"type"\tTEXT,\n' +
-		'\t"comment"\tTEXT,\n' +
-		'\t"date"\tINTEGER DEFAULT 0,\n' +
-		'\tPRIMARY KEY("id" AUTOINCREMENT)\n' +
-		");\n" +
-		"\n" +
-		'CREATE TABLE IF NOT EXISTS "inventory" (\n' +
-		'\t"id"\tINTEGER UNIQUE,\n' +
-		'\t"serial"\tTEXT UNIQUE,\n' +
-		"\t\"type\"\tTEXT DEFAULT '',\n" +
-		"\t\"model\"\tTEXT DEFAULT '',\n" +
-		'\t"status"\tINTEGER DEFAULT 0,\n' +
-		"\t\"comment\"\tTEXT DEFAULT '',\n" +
-		'\t"owner_table"\tINTEGER,\n' +
-		'\t"owner_id"\tINTEGER,\n' +
-		"\t\"smarwater\"\tTEXT DEFAULT '',\n" +
-		"\t\"brand\"\tTEXT DEFAULT '',\n" +
-		'\tPRIMARY KEY("id")\n' +
-		");\n" +
-		"\n" +
-		'CREATE TABLE IF NOT EXISTS "locker" (\n' +
-		'\t"id"\tINTEGER PRIMARY KEY AUTOINCREMENT,\n' +
-		'\t"keys"\tInteger DEFAULT 0,\n' +
-		'\t"number"\tINTEGER UNIQUE,\n' +
-		'\t"floor"\tTEXT,\n' +
-		'\t"status"\tINTEGER DEFAULT 6,\n' +
-		'\t"owner_id"\tINTEGER UNIQUE,\n' +
-		'\t"comment"\tTEXT DEFAULT ""\n' +
-		");\n" +
-		"\n" +
-		'CREATE TABLE IF NOT EXISTS "place" (\n' +
-		'\t"id"\tINTEGER UNIQUE,\n' +
-		'\t"name"\tTEXT UNIQUE,\n' +
-		'\t"comment"\tTEXT,\n' +
-		"\t\"accesspoint\"\tTEXT DEFAULT '',\n" +
-		"\t\"type\"\tTEXT DEFAULT '',\n" +
-		"\t\"casting\"\tTEXT DEFAULT '',\n" +
-		"\t\"lamptime\"\tTEXT DEFAULT '',\n" +
-		"\t\"projektor\"\tTEXT DEFAULT '',\n" +
-		"\t\"projektorContact\"\tTEXT DEFAULT '',\n" +
-		"\t\"SpeakerKontakt\"\tTEXT DEFAULT '',\n" +
-		"\t\"projektorComment\"\tTEXT DEFAULT '',\n" +
-		"\t\"cabelChannel\"\tTEXT DEFAULT '',\n" +
-		"\t\"ownerClass\"\tTEXT DEFAULT '',\n" +
-		"\t\"planning\"\tTEXT DEFAULT '',\n" +
-		'\tPRIMARY KEY("id" AUTOINCREMENT)\n' +
-		");\n" +
-		"\n" +
-		'CREATE TABLE IF NOT EXISTS "pupil" (\n' +
-		'\t"id"\tINTEGER PRIMARY KEY AUTOINCREMENT,\n' +
-		'\t"grade"\tTEXT DEFAULT "",\n' +
-		'\t"classP"\tTEXT DEFAULT "",\n' +
-		'\t"year"\tTEXT DEFAULT "",\n' +
-		'\t"firstname"\tTEXT DEFAULT "",\n' +
-		'\t"lastname"\tTEXT DEFAULT "",\n' +
-		'\t"comment"\tTEXT DEFAULT "",\n' +
-		'\t"inschool"\tINTEGER DEFAULT 1\n' +
-		");\n" +
-		"\n" +
-		'CREATE TABLE IF NOT EXISTS "type" (\n' +
-		'\t"id"\tINTEGER UNIQUE,\n' +
-		'\t"name"\tTEXT NOT NULL UNIQUE,\n' +
-		'\tPRIMARY KEY("id" AUTOINCREMENT)\n' +
-		");\n" +
-		"\n" +
-		"CREATE TABLE IF NOT EXISTS users(username TEXT primary key, password TEXT not null);\n";
+	var query = `
+		CREATE TABLE IF NOT EXISTS "history" (
+			"id"	INTEGER,
+			"owner_table"	INTEGER DEFAULT -1,
+			"owner_id"	INTEGER DEFAULT -1,
+			"type"	TEXT,
+			"comment"	TEXT,
+			"date"	INTEGER DEFAULT 0,
+			PRIMARY KEY("id" AUTOINCREMENT)
+		);
+		
+		CREATE TABLE IF NOT EXISTS "inventory" (
+			"id"	INTEGER UNIQUE,
+			"serial"	TEXT UNIQUE,
+			"type"	TEXT DEFAULT '',
+			"model"	TEXT DEFAULT '',
+			"status"	INTEGER DEFAULT 0,
+			"comment"	TEXT DEFAULT '',
+			"owner_table"	INTEGER,
+			"owner_id"	INTEGER,
+			"smarwater"	TEXT DEFAULT '',
+			"brand"	TEXT DEFAULT '',
+			PRIMARY KEY("id")
+		);
+		
+		CREATE TABLE IF NOT EXISTS "locker" (
+			"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+			"keys"	Integer DEFAULT 0,
+			"number"	INTEGER UNIQUE,
+			"floor"	TEXT,
+			"status"	INTEGER DEFAULT 6,
+			"owner_id"	INTEGER UNIQUE,
+			"comment"	TEXT DEFAULT ""
+		);
+		
+		CREATE TABLE IF NOT EXISTS "place" (
+			"id"	INTEGER UNIQUE,
+			"name"	TEXT UNIQUE,
+			"comment"	TEXT,
+			"accesspoint"	TEXT DEFAULT '',
+			"type"	TEXT DEFAULT '',
+			"casting"	TEXT DEFAULT '',
+			"lamptime"	TEXT DEFAULT '',
+			"projektor"	TEXT DEFAULT '',
+			"projektorContact"	TEXT DEFAULT '',
+			"SpeakerKontakt"	TEXT DEFAULT '',
+			"projektorComment"	TEXT DEFAULT '',
+			"cabelChannel"	TEXT DEFAULT '',
+			"ownerClass"	TEXT DEFAULT '',
+			"planning"	TEXT DEFAULT '',
+			PRIMARY KEY("id" AUTOINCREMENT)
+		);
+		
+		CREATE TABLE IF NOT EXISTS "pupil" (
+			"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+			"grade"	TEXT DEFAULT "",
+			"classP"	TEXT DEFAULT "",
+			"year"	TEXT DEFAULT "",
+			"firstname"	TEXT DEFAULT "",
+			"lastname"	TEXT DEFAULT "",
+			"comment"	TEXT DEFAULT "",
+			"inschool"	INTEGER DEFAULT 1
+		);
+		
+		CREATE TABLE IF NOT EXISTS "type" (
+			"id"	INTEGER UNIQUE,
+			"name"	TEXT NOT NULL UNIQUE,
+			PRIMARY KEY("id" AUTOINCREMENT)
+		);
+		
+		CREATE TABLE IF NOT EXISTS users(username TEXT primary key, password TEXT not null);
+		`;
 
 	db.run(query);
 }
